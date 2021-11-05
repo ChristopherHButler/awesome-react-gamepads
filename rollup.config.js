123056244fc0864aeca5ce657d5d2410f9c62c4f
Christopher Butler <christopher.harold.butler@gmail.com>
2021-11-05 22:08:14 +0100

    umd module

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,48 @@
-// import resolve from '@rollup/plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-// import commonjs from '@rollup/plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+
+import packageJSON from './package.json';
 
 
 
 const config = {
   input: './src/index.ts',
   output: [
-    // {
-    //   file: 'lib/cjs/index.js',
-    //   format: 'cjs',
-    //   name: 'ARG',
-    //   sourcemap: true,
-    // },
     {
-      file: 'lib/index.js',
+      file: packageJSON.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false,
+      // globals: {
+      //   react: "React",
+      // }
+    },
+    {
+      file: packageJSON.browser,
       format: 'umd',
       name: 'ARG',
       sourcemap: true,
+      globals: {
+        react: "React",
+      }
     },
-    // {
-    //   file: 'lib/esm/index.js',
-    //   format: 'esm',
-    //   name: 'ARG',
-    //   sourcemap: true,
-    // },
+    {
+      file: packageJSON.module,
+      format: 'esm',
+      name: 'ARG',
+      sourcemap: true,
+      globals: {
+        react: "React",
+      }
+    },
   ],
   plugins: [
     typescript(),
-    // resolve({ extensions: ['.jsx', '.js'] }),
-    // commonjs(),
+    resolve({ extensions: ['.jsx', '.js', '.tsx', 'ts'] }),
+    commonjs(),
     // json(),
   ],
   external: ['react', 'react-dom']
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { useGamepads } from './hooks/useGamepads';
-
